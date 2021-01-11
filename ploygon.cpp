25a07eaee8d0269d3e6ba587824a6024e7c9570d
yarlagaddavenkatasubbarao <yarlagadda.venkat.chinni@gmail.com>
2021-01-11 09:22:23 -0600

    Add files via upload

---
+++ ploygon.cpp
@@ -0,0 +1,13 @@
+#include<simplecpp>
+
+main_program{
+turtleSim();
+cout<<"Enter the nnumber of sides";
+int nsides;
+cin>> nsides;
+repeat(nsides){
+forward(100); right(360.0/nsides);
+wait(2);
+}
+wait(10);
+}
